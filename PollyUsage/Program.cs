2e26d6cf98f4e6d166b18e96a06ee6d86e689b96
DESKTOP-57O0UIF\qiaohengbo <529606091@qq.com>
2020-08-11 09:49:35 +0800

    添加Polly项目

---
+++ PollyUsage/Program.cs
@@ -0,0 +1,35 @@
+﻿using Polly;
+using System;
+
+namespace PollyUsage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var poliy = PolicyDemo.RegisterPolicyBuilder();
+            poliy.Execute(() => { ThrowFooException(); });
+            Console.ReadKey();
+        }
+
+        static void ThrowFooException()
+        {
+
+            Console.WriteLine("ThrowFooException");
+            throw new FooException();
+        }
+    }
+    public class PolicyDemo
+    {
+        public static Policy RegisterPolicyBuilder()
+        {
+            return Policy.Handle<FooException>().Retry(3);
+        }
+    }
+
+
+    public class FooException : Exception
+    {
+
+    }
+}
